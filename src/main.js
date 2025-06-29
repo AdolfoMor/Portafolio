5de6ec8ca3a3cc22febb2ef09054f087503952c8
Adolfo <adolfomorher@gmail.com>
2025-06-29 15:06:37 -0600

    sin analitica por ahora

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 import { createApp } from 'vue'
-import VercelAnalytics from './plugins/vercel';
 import './styles/main.css'
 import App from './App.vue'
 
-const app = createApp(App)
-
-app.use(VercelAnalytics)  // <-- registra el plugin aquí
-
-app.mount('#app')
+createApp(App).mount('#app')

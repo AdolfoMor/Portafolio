10e489ffc034404223221ad21bd7291fe24be3f1
Adolfo <adolfomorher@gmail.com>
2025-06-29 19:35:45 -0600

    mouse cel 2

--- src/Hero.vue
+++ src/Hero.vue
@@ -2,8 +2,9 @@
   <section
     ref="sectionRef"
     :class="[
-      'snap-start h-screen flex flex-col items-center justify-start md:justify-center relative bg-white dark:bg-gray-900 px-6 md:px-12 transition-all duration-700 ease-out pt-16 md:pt-0',
-      isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-6'
+      'snap-start flex flex-col items-center justify-center relative bg-white dark:bg-gray-900 px-6 md:px-12 transition-all duration-700 ease-out',
+      isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-6',
+      'min-h-[70vh] md:h-screen'
     ]"
   >
     <div class="max-w-4xl text-center z-10">
@@ -31,7 +32,7 @@
     </div>
 
     <!-- Scroll Indicator -->
-    <div class="absolute bottom-20 flex justify-center">
+    <div class="absolute bottom-12 md:bottom-20 flex justify-center">
       <div class="w-6 h-10 border-2 border-gray-400 dark:border-gray-300 rounded-full flex items-start justify-center p-1">
         <div class="w-1 h-1 bg-gray-400 dark:bg-gray-300 rounded-full animate-bounce"></div>
       </div>
